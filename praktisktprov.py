4b7e9a67ed59ef70ab0de0cd552f982962f013c1
luccur <luccur2001@skola.goteborg.se>
2025-01-13 14:16:04 +0100

    kompletterade och förbättrade koden.

--- praktisktprov.py
+++ praktisktprov.py
@@ -7,8 +7,10 @@ if (userinput == ''):
     print('Inget värde anget, stänger ner programmet.')
 else:
     number_list = userinput.split()
-    
+
     for i in range(len(number_list)):
         temp_var = int(number_list[i])
-        print(f'{temp_var*1}\n{temp_var*2}\n{temp_var*3}\n{temp_var*4}\n{temp_var*5}\n{temp_var*6}\n{temp_var*7}\n{temp_var*8}\n{temp_var*9}\n{temp_var*10}\n')
+        for i in range(1,11):
+            print(f'{temp_var} * {i} = {temp_var*i}')
+        print("")
     print('Beräkningar klara, stänger ner programmet')
